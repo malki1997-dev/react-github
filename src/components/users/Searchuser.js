bccac19acf2c0f6d167065de5a978bcdda9ba437
malki <malki.mohammed.1997@gmail.com>
2021-09-18 17:00:48 +0200

    first commit

---
+++ src/components/users/Searchuser.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class Searchuser extends Component {
+    render() {
+        return (
+            <div>
+                
+<form>
+  
+  <div class="form-group">
+    <label for=""></label>
+    <input type="text" name="" id="" class="form-control" />
+    
+  </div>
+
+
+</form>
+
+
+            </div>
+        )
+    }
+}
+
+export default Searchuser
